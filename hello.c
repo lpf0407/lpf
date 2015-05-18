75f6b640734e7b2910b71d01f97096d2e7d6bfba
Li Pengfei <lipf0407@Thundersoft.com>
2015-05-18 17:33:35 +0800

    hello.c

---
+++ hello.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  05/18/2015 05:31:14 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include<stdio.h>
+int main()
+{
+        printf("Hello\n");
+    return 0;
+}
